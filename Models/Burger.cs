2c269791cc53ec4bcfd884cf2869f5f808ea570b
Kamila <juliana.sosa@udla.edu.ec>
2024-04-23 19:41:43 -0500

    Creacion de la clase Burgers y definicion de sus atributos

---
+++ Models/Burger.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrePrueba.Models
+{
+    public class Burger
+    {
+        public int BurgerID { get; set; }
+        [Required]
+        public string? Nombre {  get; set; }        
+        public bool ConQueso {  get; set; }
+        public decimal Precio { get; set; }   
+
+    }
+}
