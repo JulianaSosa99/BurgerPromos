cd0e83e9f8781bce1d005c6c851d7f23eaae7810
Kamila <juliana.sosa@udla.edu.ec>
2024-04-23 21:58:18 -0500

    Creacion de la validación DataAnnotation personalizada mediante la clase ValidationAtributte y tambien con el metodo isValid con el fin de luego crear validaciones de rango de precio en este caso no debe ser menor a 20

--- Models/Burger.cs
+++ Models/Burger.cs
@@ -9,8 +9,28 @@ namespace PrePrueba.Models
         public string? Nombre { get; set; }
         public bool ConQueso { get; set; }
 
+        [VerificarRango]
         public decimal Precio { get; set; }
         public List<Promo>? Promo { get; set; }
+    }
+    public class VerificarRango : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            decimal valor = (decimal)value;
+            if (valor > 20)
+            {
+                return false;
+
+            }
+            else
+            {
+                return base.IsValid(value);
+            }
+
+            
+
+        }
     }
 
 }
